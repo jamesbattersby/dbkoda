08056785088d9594f4d91bb88300ad21b603a3f5
James Battersby <james.battersby@mediakind.com>
2024-08-31 13:17:18 +0100

    Update build script

--- buildFromSource.sh
+++ buildFromSource.sh
@@ -1,10 +1,10 @@
 #  Pre-requisities: node, npm
 
 npm -g install yarn rimraf
- 
-git clone https://github.com/SouthbankSoftware/dbkoda
-git clone https://github.com/SouthbankSoftware/dbkoda-ui
-git clone https://github.com/SouthbankSoftware/dbkoda-controller
+
+git clone https://github.com/jamesbattersby/dbkoda
+git clone https://github.com/jamesbattersby/dbkoda-ui
+git clone https://github.com/jamesbattersby/dbkoda-controller
 
 
 
@@ -13,12 +13,12 @@ git clone https://github.com/SouthbankSoftware/dbkoda-controller
 for dir in dbkoda-ui dbkoda-controller dbkoda ;do
    ( cd $dir;yarn install  )
 done
- 
+
 (cd dbkoda;yarn dev:link)
 
-(cd dbKoda-controller; npm rebuild) 
+(cd dbKoda-controller; npm rebuild)
 
-(cd  dbKoda-ui;yarn dev) & 
-(cd  dbKoda-controller; yarn dev) & 
-(cd  dbKoda; npm run byo) & 
+(cd  dbKoda-ui;yarn dev) &
+(cd  dbKoda-controller; yarn dev) &
+(cd  dbKoda; npm run byo) &
 
